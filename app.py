96adcb62b0a71f8907f919fdc380972f027bf316
Kostya <konstantin.morozov@here.com>
2020-06-19 22:23:46 +0300

    counter

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 from datetime import datetime
 
 app = Flask(__name__)
 
+class Vars(object):
+    total = 0
+
+    def __init__(self):
+        Vars.total+=1
+
+    @staticmethod
+    def status():
+        return str(Vars.total)
+
+
 @app.route('/')
 def hello_world():
-    curr_time = datetime.now()
-    return str(curr_time)
+    me = Vars()
+    if request.method == "GET":
+        vari = me.status()
+        curr_time = datetime.now()
+        response = f'Calls: {vari}, {curr_time}'
+        return response
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
